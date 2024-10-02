c7e8e90932d3f9d537041f549d067dbc1971fdad
@JonatasAriel <jonatas.cruz@jala.university>
2024-10-01 23:31:51 -0300

    Criação da classe de de alimentos Criaçao da funçao pra extrair data

---
+++ Alimento-class.cs
@@ -0,0 +1,15 @@
+public class Alimento
+{
+    public string Codigo { get; set; }
+    public string Nome { get; set; }
+    public string NomeCientifico { get; set; }
+    public string Grupo { get; set; }
+    public List<ComponenteNutricional> Componentes { get; set; } = new List<ComponenteNutricional>();
+}
+
+public class ComponenteNutricional
+{
+    public string Nome { get; set; }
+    public decimal Valor { get; set; }
+    public string Unidade { get; set; }
+}
